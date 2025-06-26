6a721dd50570e56ba21b6d8d6b422aa3d4f1610a
Alain Bindele <alain.bindele@gmail.com>
2025-06-27 01:19:03 +0200

    Updated Query.java

--- src/main/java/com/notifyme/entity/Query.java
+++ src/main/java/com/notifyme/entity/Query.java
@@ -23,9 +23,21 @@ public class Query {
     
     @Column(columnDefinition = "TEXT", nullable = false)
     private String prompt;
-    
+
+    @Column(name = "closed")
+    private Boolean closed = false;
+
     @Column(name = "is_valid")
     private Boolean isValid = false;
+
+    @Column(name = "cron")
+    private Boolean cron = false;
+
+    @Column(name = "date_specific")
+    private Boolean dateSpecific = false;
+
+    @Column(name = "to_check")
+    private Boolean toCheck = false;
     
     @Column(name = "cron_params", length = 30)
     private String cronParams;
@@ -36,11 +48,24 @@ public class Query {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
     
+    @Column(name = "valid_from")
+    private LocalDateTime validFrom;
+    
+    @Column(name = "valid_to")
+    private LocalDateTime validTo;
+
+    @Column(name = "enabled_channels")
+    private String enabledChannels;
+
+
+    
     @OneToMany(mappedBy = "query", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Execution> executions;
     
     @OneToMany(mappedBy = "query", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Notification> notifications;
+
+
     
     public Query(User user, String prompt) {
         this.user = user;
