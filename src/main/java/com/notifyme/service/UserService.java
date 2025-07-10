c5224b5cd9a49fc2b40399dfbe0ec52862054d79
Alain Bindele <alain.bindele@gmail.com>
2025-07-11 00:47:20 +0200

    Fix Missing Optional Import

--- src/main/java/com/notifyme/service/UserService.java
+++ src/main/java/com/notifyme/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
