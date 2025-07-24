def4cfd1079e4fb39726925950dabc34a908aa04
Alain Bindele <alain.bindele@gmail.com>
2025-07-25 00:40:10 +0200

    Fix import Jackson annotations

--- src/main/java/com/notifyme/entity/TQuery.java
+++ src/main/java/com/notifyme/entity/TQuery.java
@@ -1,5 +1,7 @@
 package com.notifyme.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
