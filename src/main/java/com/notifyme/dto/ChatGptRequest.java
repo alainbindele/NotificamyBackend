d9f8eaf4dae0cb7cd56ac714eb17b9cd846745d6
Alain Bindele <alain.bindele@gmail.com>
2025-07-25 13:56:58 +0200

    Risolvi costruttore duplicato in ChatGptRequest

--- src/main/java/com/notifyme/dto/ChatGptRequest.java
+++ src/main/java/com/notifyme/dto/ChatGptRequest.java
@@ -1,13 +1,11 @@
 package com.notifyme.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ChatGptRequest {
     
     @JsonProperty("policy")
